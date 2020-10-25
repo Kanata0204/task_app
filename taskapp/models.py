6d863aea8706bf01a19873fd7d9235c198db3590
kanata <kanahira24@outlook.jp>
2020-10-25 15:07:38 +0900

    add "time_limit", "memo" in model, form
    but, "time_limit" field don't reflect to model

--- taskapp/models.py
+++ taskapp/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Task(models.Model):
-    content = models.CharField(max_length=140)
-    posted_date = models.DateTimeField(auto_now_add=True)
-    
+    title = models.CharField(max_length=140)
+    time_limit = models.DateTimeField(null=True, blank=True)
+    memo = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
