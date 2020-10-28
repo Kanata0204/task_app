dd84ed7f399f81b0ec50d8e24e0c73e959cb2dfb
kanata <kanahira24@outlook.jp>
2020-10-28 15:46:51 +0900

    implement time_limit and day_limit of form.py

--- taskapp/models.py
+++ taskapp/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-
 # Create your models here.
 
 class Task(models.Model):
-    title = models.CharField(max_length=140)
-    time_limit = models.DateTimeField(null=True, blank=True)
-    memo = models.TextField(null=True, blank=True)
+    title = models.CharField( verbose_name="タスク名", max_length=140)
+    day_limit = models.DateField(null=True, blank=True, verbose_name="期限日")
+    time_limit = models.TimeField(null=True, blank=True, verbose_name="時間の期限")
+    memo = models.TextField(null=True, blank=True,  verbose_name="補足情報")
 
     def __str__(self):
         return self.title
