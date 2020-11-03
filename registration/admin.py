058ab1c01fd6b378875f86fd137dbd699c360d37
kanata <kanahira24@outlook.jp>
2020-11-03 23:01:28 +0900

    implement customuser

--- registration/admin.py
+++ registration/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+#  your models here.
+admin.site.register(User, UserAdmin)
