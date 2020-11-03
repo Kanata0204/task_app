6be9fc12dd326f23571ca05a34456f42d8503dc1
kanata <kanahira24@outlook.jp>
2020-11-03 23:01:28 +0900

    implement customuser

--- taskproject/settings.py
+++ taskproject/settings.py
@@ -122,6 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+AUTH_USER_MODEL = 'registration.User'
+
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
--- taskproject/urls.py
+++ taskproject/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from django.contrib.auth.models import Group
+
+admin.site.unregister(Group)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
